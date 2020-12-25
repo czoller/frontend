f2b739179e679e3bfe1cdc29c4884c31f4704e92
czoller <czoller@users.noreply.github.com>
2020-12-25 23:30:45 +0100

    New Zuul project from Spring Initializr

---
+++ src/test/java/com/github/acme42/frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.acme42.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
