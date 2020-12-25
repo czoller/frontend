291db110ffd8839a8ed54c74feaf692f507f7b52
czoller <czoller@users.noreply.github.com>
2020-12-25 23:35:56 +0100

    Configured Zuul

--- src/main/java/com/github/acme42/frontend/FrontendApplication.java
+++ src/main/java/com/github/acme42/frontend/FrontendApplication.java
@@ -2,8 +2,10 @@ package com.github.acme42.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class FrontendApplication {
 
 	public static void main(String[] args) {
